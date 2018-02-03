0fa53bcbfaa0d87eee3b5996bd1bf053ce7ab135
Dominik Meißner <dominik.meissner@uni-ulm.de>
2018-02-04 00:29:08 +0100

    remove anonymous mode again

--- bin/distributed-barrier.js
+++ bin/distributed-barrier.js
@@ -10,12 +10,6 @@ const port = process.env.PORT || 8413;
 const waitSet = new Set(process.argv.slice(2));
 const connections = new Map();
 
-const anonymousMode = process.argv[2] === '-n';
-let awaiting = waitSet.size;
-if (anonymousMode) {
-    awaiting = parseInt(process.argv[3]);
-}
-
 const checkSet = () => {
     if (waitSet.size === 0) {
         for (let connection of connections.keys()) {
@@ -28,52 +22,25 @@ const checkSet = () => {
 
 const server = net.createServer(socket => {
     console.log(`[debug] ${socket.remoteAddress}:${socket.remotePort}`);
-
-    if (anonymousMode) {
-        connections.set(socket, awaiting);
-        awaiting -= 1;
-
-        console.log(`[join] awaiting ${awaiting}`);
-
-        if (awaiting === 0) {
-            for (let connection of connections.keys()) {
-                connections.delete(connection);
-                connection.end('Reached barrier!\n');
-            }
-            server.close();
+    socket.on('data', data => {
+        const token = data.toString().trim();
+        if (waitSet.has(token)) {
+            console.log(`[join] ${token}`);
+            waitSet.delete(token);
+            connections.set(socket, token);
+            checkSet();
+        } else {
+            socket.end('Invalid token!\n');
         }
-
-        socket.on('end', () => {
-            if (connections.has(socket)) {
-                const token = connections.get(socket);
-                console.log(`[leave] awaiting ${awaiting}`);
-                connections.delete(socket);
-                awaiting += 1;
-            }
-        });
-    } else {
-        socket.on('data', data => {
-            console.log(`[debug] ${data.toString()}`);
-
-            const token = data.toString().trim();
-            if (waitSet.has(token)) {
-                console.log(`[join] ${token}`);
-                waitSet.delete(token);
-                connections.set(socket, token);
-                checkSet();
-            } else {
-                socket.end('Invalid token!\n');
-            }
-        });
-        socket.on('end', () => {
-            if (connections.has(socket)) {
-                const token = connections.get(socket);
-                console.log(`[leave] ${token}`);
-                waitSet.add(token);
-                connections.delete(socket);
-            }
-        });
-    }
+    });
+    socket.on('end', () => {
+        if (connections.has(socket)) {
+            const token = connections.get(socket);
+            console.log(`[leave] ${token}`);
+            waitSet.add(token);
+            connections.delete(socket);
+        }
+    });
 });
 
 server.listen(port);
